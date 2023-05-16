e1b959bcbbc144b50f53f29d03a7216ad960ff33
Jialin Lu <1640889387@qq.com>
2023-05-16 08:13:49 +0800

    更新C#教程 0～1章

---
+++ Documents/C#教程/App.cs
@@ -0,0 +1,19 @@
+﻿// 1. 用using导入系统库
+using System;
+
+// 2. 用namespace定义命名空间
+namespace DEMO
+{
+    // 3. 定义调用Main方法的程序入口类
+    // 命名空间内还可以定义其他类、接口、枚举等
+    class App
+    {
+        // 4. 定义程序最先执行的Main方法
+        // 还可以定义其他方法、属性等
+        static void Main(string[] args)
+        {
+            // 项目开始运行...
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
